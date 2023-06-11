860709e29f61fc763f79714e71b8b5b13dea0f2a
Ch3k00 <129698481+Ch3k00@users.noreply.github.com>
2023-06-11 18:22:03 -0400

    appjs nuevo

--- T2/src/app.js
+++ T2/src/app.js
@@ -31,10 +31,98 @@ app.get('/users/:id', UsersController.getUserById)
 app.post('/users', UsersController.createUser)
 app.get('/users/:id/posts', UsersController.usersPosts)
 
+route.get("/top5personajesConMasFuerza",async (req,res) => {
+    try{
+        const personajes = await prisma.personaje.findMany({
+            take:5,
+            orderBy:{ fuerza: "desc"
+    
+        }
+    });
+    res.json(personajes);
+    }
+    catch(error){
+        console.error(error);
+        res.status(500).json({error: "Error al obtener los personajes con mas fuerza"});
+    }
+});
+
+route.get("/cantidadHabitantes/:id_reino", async (req,res) => {
+const id_reino = Number(req.params.id_reino)
+
+try {
+  const cantidadHabitantes = await prisma.personaje_habita_reino.count({
+    where: {
+      id_reino: id_reino
+    }
+  });
+
+  res.json({ cantidadHabitantes });
+} catch (error) {
+  console.error(error);
+  res.status(500).json({ error: 'Error al obtener la cantidad de habitantes del reino.' });
+}
+});
+
+route.get("/gobernante/:id_reino?", async (req,res) =>{
+    const id_reino = req.params.id_reino ? Number(req.params.id_reino) : null;
+    try {
+      let gobernantes;
+        if (id_reino) {
+        gobernantes = await prisma.personaje_habita_reino.findMany({
+          where: {
+            id_reino: id_reino,
+            es_gobernante: true
+          },
+          select: {
+            id_personaje: true
+          }
+        });
+      } else {
+        gobernantes = await prisma.personaje_habita_reino.findMany({
+          where: {
+            es_gobernante: true
+          },
+          select: {
+            id_personaje: true,
+            id_reino:true
+          }
+        });
+      }
+        res.json({ gobernantes });
+      
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Error al obtener los gobernantes del reino.' });
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //psj
 route.post("/personaje",personaje.crearPersonaje)
 route.get("/personaje",personaje.obtenerPersonaje)
-route.get("/personaje",personaje.obtenerPersonajePorId)
+route.get("/personaje/:id",personaje.obtenerPersonajePorId)
 route.put("/personaje", personaje.actualizarPersonaje)
 route.delete("/personaje",personaje.eliminarPersonaje)
 
@@ -42,7 +130,7 @@ route.delete("/personaje",personaje.eliminarPersonaje)
 
 route.post("/karts",karts.crearKart)
 route.get("/karts",karts.obtenerKart)
-route.get("/karts",karts.obtenerKartId)
+route.get("/karts/:id",karts.obtenerKartId)
 route.put("/karts",karts.actualizarKart)
 route.delete("/karts",karts.eliminarKart)
 
@@ -50,35 +138,35 @@ route.delete("/karts",karts.eliminarKart)
 
 route.post("/personaje_habita_reino",personaje_habita_reino.crearPersonajeHabitaReino)
 route.get("/personaje_habita_reino",personaje_habita_reino.obtenerPersonajeHabitaReino)
-route.get("/personaje_habita_reino",personaje_habita_reino.obtenerPersonajeHabitaReinoPorId)
+route.get("/personaje_habita_reino/:id_reino/:id_personaje",personaje_habita_reino.obtenerPersonajeHabitaReinoPorId)
 route.put("/personaje_habita_reino",personaje_habita_reino.actualizarPersonajeHabitaReino)
 route.delete("/personaje_habita_reino",personaje_habita_reino.eliminarPersonajeHabitaReino)
 
 //personaje tiene trabajo
 route.post("/personaje_tiene_trabajo",ptts.crearPtt)
 route.get("/personaje_tiene_trabajo",ptts.obtenerPtt)
-route.get("/personaje_tiene_trabajo",ptts.obtenerPttId)
+route.get("/personaje_tiene_trabajo/:id_personaje/:id_trabajo",ptts.obtenerPttId)
 route.put("/personaje_tiene_trabajo",ptts.actualizarPtt)
 route.delete("/personaje_tiene_trabajo",ptts.eliminarPtt)
 
 //trabajos 
 route.post("/trabajos",trabajos.crearTrabajo)
 route.get("/trabajos",trabajos.obtenerTrabajo)
-route.get("/trabajos",trabajos.obtenerTrabajoPorId)
+route.get("/trabajos/:id",trabajos.obtenerTrabajoPorId)
 route.put("/trabajos",trabajos.actualizarTrabajo)
 route.delete("/trabajos",trabajos.eliminarTrabajo)
 
 //reinos
 route.post("/reinos",reino.crearReino)
 route.get("/reinos",reino.obtenerReino)
-route.get("/reinos",reino.obtenerReinoPorId)
+route.get("/reinos/:id",reino.obtenerReinoPorId)
 route.put("/reinos",reino.actualizarReino)
 route.delete("/reinos",reino.eliminarReino)
 
 //diplomacias
 route.post("/diplomacias",diplomacia.crearDiplo)
 route.get("/diplomacias",diplomacia.obtenerDiplo)
-route.get("/diplomacias",diplomacia.obtenerDiploId)
+route.get("/diplomacias/:id_1/:id_2",diplomacia.obtenerDiploId)
 route.put("/diplomacias",diplomacia.actualizarDiplo)
 route.delete("/diplomacias",diplomacia.eliminarDiplo)
 
@@ -86,14 +174,14 @@ route.delete("/diplomacias",diplomacia.eliminarDiplo)
 //reinos defensas
 route.post("/reinos_defensas",reinosDefensas.crearReinoDef)
 route.get("/reinos_defensas",reinosDefensas.obtenerReinoDef)
-route.get("/reinos_defensas",reinosDefensas.obtenerReinoDefPorId)
+route.get("/reinos_defensas/:id_reinos/:id_defensas",reinosDefensas.obtenerReinoDefPorId)
 route.put("/reinos_defensas",reinosDefensas.actualizarReinoDef)
 route.delete("/reinos_defensas",reinosDefensas.eliminarReinoDef)
 
 //defensas
 route.post("/defensas",defensas.crearDefensa)
 route.get("/defensas",defensas.obtenerDefensa)
-route.get("/defensas",defensas.obtenerDefensaPorId)
+route.get("/defensas/:id",defensas.obtenerDefensaPorId)
 route.put("/defensas",defensas.actualizarDefensa)
 route.delete("/defensas",defensas.eliminarDefensa)
 //==========================================================//
